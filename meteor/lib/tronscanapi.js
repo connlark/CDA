7fa94b8537bff1bdd778dd33d8cd18b0d433255e
Connor Larkin <connor.larkin1@gmail.com>
2019-03-12 01:52:13 -0500

    oops

--- meteor/lib/tronscanapi.js
+++ meteor/lib/tronscanapi.js
@@ -1,3 +1,12 @@
+const TronWeb = require('tronweb')
+
+// This provider is optional, you can just use a url for the nodes instead
+const HttpProvider = TronWeb.providers.HttpProvider;
+const fullNode = new HttpProvider('https://api.trongrid.io'); // Full node http endpoint
+const solidityNode = new HttpProvider('https://api.trongrid.io'); // Solidity node http endpoint
+const eventServer = new HttpProvider('https://api.trongrid.io'); // Contract events http endpoint
+
+const privateKey = 'da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0';
 
 const tronWeb = new TronWeb(
     fullNode,
